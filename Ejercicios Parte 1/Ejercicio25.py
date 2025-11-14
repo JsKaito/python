c4d0d30fe98e0849f2f1a8085596705e3d63ae45
kaito <fernandoruizmurciano@gmail.com>
2025-11-14 20:21:22 +0100

    25 terminado. Hecho el 26. Parte 2 terminada. Falta P3.

--- Ejercicios Parte 1/Ejercicio25.py
+++ Ejercicios Parte 1/Ejercicio25.py
@@ -1,15 +1,32 @@
-# Esta vez he decidido intentar usar un diccionario con triplas para optimizar el código.add()
+# Esta vez he decidido intentar usar un diccionario con triplas para optimizar el código.add(), usándolo como una especie de control switch.
 # Es parecido pero con facultades en vez de días de la semana.
 
 facultades = {
-    1: ("Ingeniería de Sistemas", 350, 650),
-    2: ("Derecho", 300, 550),
-    3: ("Ingeniería Naviera", 300, 500),
-    4: ("Ingeniería Pesquera", 310, 460),
-    5: ("Contabilidad", 280, 490),
-    6: ("Administración", 360, 520)
+    1:("Ingeniería de Sistemas", 350, 650),
+    2:("Derecho", 300, 550),
+    3:("Ingeniería Naviera", 300, 500),
+    4:("Ingeniería Pesquera", 310, 460),
+    5:("Contabilidad", 280, 490),
+    6:("Administración", 360, 520)
     }
     
-while True:
-    str(input("Ingresa tu nombre:"))
+nombre = str(input("Ingresa tu nombre: "))
     
+print("\nFacultades:")
+    
+for numero, datos in facultades.items():
+    print(f"{numero}. {datos[0]}") # Datos contiene el nombre, la matrícula y la mensualidad.
+    
+while True:
+    num = int(input("\nIngresa tu facultad (1-6): "))
+    if num in facultades:
+        break
+    
+    print("Número de facultad incorrecto. Debe estar entre 1 y 6.")
+    
+facultad, matricula, mensualidad = facultades[num]
+igv = (mensualidad + matricula) * 0.18
+
+print(f"\n{nombre}, tu matrícula es de {matricula}, tu mensualidad de {mensualidad} y tu IGV es de {igv}.")
+
+print(f"\nTu coste total es {matricula + mensualidad + igv} €")
---
+++ Ejercicios Parte 1/Ejercicio26.py
@@ -0,0 +1,44 @@
+import random
+
+dados = [0, 0, 0]
+
+def tirarDados(dados):
+    for i in range(len(dados)):
+        dados[i] = random.randint(1, 6)
+    return dados
+
+def calcResultado(dados):
+    cont = 0
+    
+    for i in range(len(dados)):
+        if dados[i] == 6:
+            cont += 1
+    
+    if cont == 3:
+        return "¡Excelente!"
+    elif cont == 2:
+        return "¡Muy bien!"
+    elif cont == 1:
+        return "¡Regular!"
+    elif cont == 0:
+        return "¡Pésimo!"
+
+tirarDados(dados)    
+calcResultado(dados)
+
+print(calcResultado(dados),"Tus resultados han sido:", dados)
+
+
+
+'''
+Función para testear si la lógica es correcta que he usado para testear (Mira cuantas tiradas tarda en sacar triple 6):
+
+    tiradas = 0
+
+    while not dados == [6, 6, 6]:
+        tirarDados(dados)
+        tiradas += 1
+        
+    print(tiradas)
+
+'''
