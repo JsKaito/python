3dcc2cd85b2fdb85b5b702145eda046a642fdb7b
kaito <fernandoruizmurciano@gmail.com>
2025-11-18 16:02:54 +0100

    arreglado ej 2

--- Ejercicios (17-11-2025)/Ejercicio2.py
+++ Ejercicios (17-11-2025)/Ejercicio2.py
@@ -1,7 +1,7 @@
-#altura = 5
-altura = int(input("Introduce la altura de la escalera: "))
+altura = 5
+#altura = int(input("Introduce la altura de la escalera: "))
 print (4)
-for i in range (1, altura):
-    print(4, ' '*i, '4', sep='')
-    if i == altura-1:
-        print('4'*(i+3))
+for i in range (1, altura-1):
+    print(4, ' '*(i-1), '4', sep='')
+    if i == altura -2:
+        print('4'*(i+2))
