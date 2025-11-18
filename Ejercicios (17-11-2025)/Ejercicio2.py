9b2944e090d23c83c3ec3013ba4d83abcbee53ac
kaito <fernandoruizmurciano@gmail.com>
2025-11-18 16:05:41 +0100

    mejorada la lógica del ejercicio 2

--- Ejercicios (17-11-2025)/Ejercicio2.py
+++ Ejercicios (17-11-2025)/Ejercicio2.py
@@ -1,7 +1,8 @@
-altura = 5
-#altura = int(input("Introduce la altura de la escalera: "))
-print (4)
-for i in range (1, altura-1):
-    print(4, ' '*(i-1), '4', sep='')
-    if i == altura -2:
-        print('4'*(i+2))
+altura = int(input("Introduce la altura de la escalera: "))
+for i in range (1, altura+1):
+    if i == 1:
+        print('4'*i)
+    elif i == altura:
+        print('4'*i)
+    else:
+        print('4' + ' '*(i-2) + '4')
