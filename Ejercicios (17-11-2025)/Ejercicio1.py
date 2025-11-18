5c6b28ab00852bcc5f937802d3c46d257dfd7873
kaito <fernandoruizmurciano@gmail.com>
2025-11-18 16:23:51 +0100

    Mejorada la lógica y entrada del ejercicio 1.

--- Ejercicios (17-11-2025)/Ejercicio1.py
+++ Ejercicios (17-11-2025)/Ejercicio1.py
@@ -1,12 +1,23 @@
-#altura = 5
-altura = int(input("Introduce la altura de la escalera: "))
+altura = int((input("Introduce la altura de la mitad de la pirámide horizontal. El número debe ser impar: ")))//2
 
-print(' '*altura, '*', sep='') # Punta alta
 
-for i in range(1, altura+1):
-    print(' '*(altura-i), '*', ' '*(i-1), '*', sep='') # Tríangulo alto (La última fila hace de punto medio y se corrige abajo)
 
-for i in range(altura, 1, -1):
-    print(' '*(altura+1 - i), '*', ' '*(i-2), '*', sep='') # Triángulo bajo
+# Mitad Superior
+for i in range (1, altura + 1):
     
-print(' '*altura, '*', sep='') #Punta baja
+    if i == 1:
+        print(' ' * (altura), '*', sep= '') # Punta alta
+    else:
+        print(' ' * (altura - i +1), '*', (i-2) * ' ', '*', sep ='') # Resto del triángulo
+
+
+# Conexión
+print('*', ' ' * (altura - 1), '*', sep = '')
+
+# Mitad Inferior
+for i in range (altura, 0, -1): # Invierto el bucle por completo, pasando del rango (1, altura + 1), subiendo a (altura, 1-1), bajando.
+    
+    if i == 1:
+        print(' ' * (altura), '*', sep= '') # Punta baja
+    else:
+        print(' ' * (altura - i +1), '*', (i-2) * ' ', '*', sep ='') # Resto del triángulo
--- Ejercicios (17-11-2025)/Ejercicio2.py
+++ Ejercicios (17-11-2025)/Ejercicio2.py
@@ -1,5 +1,7 @@
 altura = int(input("Introduce la altura de la escalera: "))
+
 for i in range (1, altura+1):
+    
     if i == 1:
         print('4'*i)
     elif i == altura:
